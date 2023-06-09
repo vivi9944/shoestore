6a7724690381e98a35d4ae23b83d0c6f377f0ba2
vivi9944 <113431657+vivi9944@users.noreply.github.com>
2023-06-09 19:14:57 +0530

    Add files via upload

---
+++ shoestore/src/main/java/com/example/shoestore/ShoestoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.shoestore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoestoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoestoreApplication.class, args);
+	}
+
+}
---
+++ shoestore/src/test/java/com/example/shoestore/ShoestoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shoestore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoestoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
